a0dd1e496382dfa04320ae6706711240010e018d
Steven Daniel <danielsteven.ds@gmail.com>
2024-07-13 20:03:37 +0300

    Update 04_underscores.py

--- 19_OOP/04_underscores.py
+++ 19_OOP/04_underscores.py
@@ -1,14 +1,13 @@
 class Person:
     def __init__(self):
         self.name = "Steven"
-        self._secret = "This is a secret"
+        self._secret = "This is a secret" #private attribute
     # def doorman(self, guess):
     #     if guess == self._secret:
     #         return "Access granted"
     #     else:
     #         return "Access denied"
-        self.__msg = "This is a private message"
-
+        self.__msg = "This is a private message" # name mangling to make it private
 
 p = Person()
 
